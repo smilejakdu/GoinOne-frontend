29f31209bff3ae1bc6b2f9263a0d54d1a7eb0b29
pgs787 <pgs787@naver.com>
2020-03-12 22:35:11 +0900

    피드백 적용 완료 (오타 수정, util 안에 정규식 변수화, 스타일컴포넌트 수정

---
+++ src/util/regexp.js
@@ -0,0 +1,8 @@
+// 이메일 정규식
+export const check_email = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+// 대문자 정규식
+export const upper_case = /[A-Z]/;
+// 소문자 정규식
+export const lower_case = /[a-z]/;
+// 숫자 정규식
+export const number_case = /\d/;
